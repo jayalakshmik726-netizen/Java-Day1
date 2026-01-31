c593e4a3522f0c0ba37bb4ec5cfa629e89326db0
jayalakshmik726-netizen <jayalakshmik726@gmail.com>
2026-01-31 14:35:42 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,7 @@
+public class Demo {
+    public static void main(String[]args){
+     System.out.print("welcom to java");
+     System.out.println("java");
+     System.out.print("cse");   
+    }
+}
---
+++ Test.java
@@ -0,0 +1,14 @@
+public class test{
+    public static void main(String[] args){
+        int a=20;
+        int b=40;
+        if(a>b){
+            System.out.println("a is max");
+        }
+        else{
+            System.out.println("b is max");
+        }
+        }
+    }
+    
+
