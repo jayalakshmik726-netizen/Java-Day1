6cd904643525cac64a464c862383c393c71d18f7
jayalakshmik726-netizen <jayalakshmik726@gmail.com>
2026-01-31 15:29:44 +0530

    Add files via upload

---
+++ AGE.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class AGE{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int age=sc.nextInt();
+        System.out.println(age);
+    }
+}
